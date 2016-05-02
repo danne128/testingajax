df39c65a201f8d3d1fc61629053d062cfa5664ce
danne128 <daniel.trondsen.wallin@yh.nackademin.se>
2016-05-02 11:38:11 +0200

    edited js file

--- ajax.js
+++ ajax.js
@@ -1,3 +1,16 @@
 // Javascript med AJAX
+// Vi använder AJAX för att hämta innehållet på en hemsida
+// Ajax hämtar data från någon address
 
-alert("HEJSAN")
+// Påbörja en hämtning
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+
+    console.log("Nånting hände!");
+
+}
+
+request.open('GET', 'http://www.google.com');
+request.send();
